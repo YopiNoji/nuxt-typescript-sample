89aaa9a38f4ce1cf8c2ef4bbeb30798258e5d6ee
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-04-16 23:49:05 +0900

    add counter component

---
+++ components/composition/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="increment">Increment ++</button>
+    <p>{{ count }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent, ref } from '@vue/composition-api'
+
+// カウンターに関わるものを関数でまとめる
+const useCounter = () => {
+  const count = ref(0)
+  const increment = () => {
+    count.value++
+  }
+  return { count, increment }
+}
+
+export default createComponent({
+  setup() {
+    const { count, increment } = useCounter()
+    return { count, increment }
+  }
+})
+</script>
+
+<style>
+
+</style>
--- pages/composition.vue
+++ pages/composition.vue
@@ -2,14 +2,26 @@
   <div class="container">
     <h2>{{ state.messageOne }}</h2>
     <h2>{{ messageTwo }}</h2>
+    <counter />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, onMounted, onUpdated, onUnmounted } from '@vue/composition-api'
+import {
+  defineComponent,
+  reactive,
+  ref,
+  onMounted,
+  onUpdated,
+  onUnmounted
+} from '@vue/composition-api'
+import Counter from '~/components/composition/Counter.vue'
 
 export default defineComponent({
   layout: 'menu',
+  components: {
+    Counter
+  },
   setup() {
     onMounted(() => {
       console.log('mounted!')
